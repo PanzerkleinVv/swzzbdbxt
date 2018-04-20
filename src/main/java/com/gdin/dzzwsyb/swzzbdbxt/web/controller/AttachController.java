9b3ca464c71d9fa30d7ccc7cf49c87e1a7a6c7e7
hpgithubs1 <630519137@qq.com>
2018-04-20 09:08:16 +0800

    修改attachcontroller

--- src/main/java/com/gdin/dzzwsyb/swzzbdbxt/web/controller/AttachController.java
+++ src/main/java/com/gdin/dzzwsyb/swzzbdbxt/web/controller/AttachController.java
@@ -12,13 +12,73 @@ import com.gdin.dzzwsyb.swzzbdbxt.web.model.Attach;
 public class AttachController {
 
 	@RequestMapping(value = "/upload")
-	public String upload(Model model, HttpServletRequest request) {
-		return null;
+	public String upload(Model model,   @RequestParam("file")MultipartFile multipartFile,HttpServletRequest request) throws IllegalStateException, IOException  {
+		
+			if(multipartFile.getSize()>0) {
+				String fileName = multipartFile.getOriginalFilename();
+				System.out.println(multipartFile.getOriginalFilename());
+				//保存的位置
+				//String path = request.getSession().getServletContext().getRealPath("files/");
+				File filepath = new File("C://Users//Administrator//git//swzzbdbxt//WebContent//files",fileName);
+				 if (!filepath.getParentFile().exists()) { 
+					 filepath.getParentFile().mkdirs();
+		            } 	
+				multipartFile.transferTo(filepath);
+			}
+			//没有选中文件，返回错误页面
+			else {
+				System.out.println(multipartFile.getOriginalFilename());
+				return "/500";
+			}
+		
+		return "/401";
 	}
 	
 	@RequestMapping(value = "/download")
-	public String download(Model model, Attach attach) {
-		return null;
+	public String download(Model model, Attach attach,HttpServletRequest request,HttpServletResponse response) throws IOException {
+		String filename = attach.getAttachFileName(); 
+		 if (filename != null) {
+			 File filepath = new File("C://Users//Administrator//git//swzzbdbxt//WebContent//files",filename);
+			 if (filepath.exists()) {
+				 response.setContentType("application/force-download");// 设置强制下载不打开
+			     response.addHeader("Content-Disposition",
+			                         "attachment;fileName=" + filename);// 设置文件名
+			     byte[] buffer = new byte[1024];
+			     FileInputStream fis = null;
+			     BufferedInputStream bis = null;
+			     try {
+			    	 fis = new FileInputStream(filepath);
+			    	 bis = new BufferedInputStream(fis);
+			    	 OutputStream os = response.getOutputStream();
+			    	 int i = bis.read(buffer);
+			    	 while (i != -1) {
+			    		 os.write(buffer, 0, i);
+			    		 i = bis.read(buffer);
+			                   }
+			      } catch (Exception e) {
+			                      // TODO: handle exception
+			    	  e.printStackTrace();
+			      } finally {
+			    	  		if (bis != null) {
+			    	  			try {
+			                             bis.close();
+			                         } catch (IOException e) {
+			                             // TODO Auto-generated catch block
+			                            e.printStackTrace();
+			                         }
+			                     }
+			    	  		if (fis != null) {
+			    	  			try {
+			                             fis.close();
+			                         } catch (IOException e) {
+			                             // TODO Auto-generated catch block
+			                             e.printStackTrace();
+			                          }
+			                     }
+			                  }
+			              }
+			         }
+		return "/500";  
 	}
 	
 	@RequestMapping(value = "/delete")
@@ -27,3 +87,4 @@ public class AttachController {
 	}
 
 }
+
