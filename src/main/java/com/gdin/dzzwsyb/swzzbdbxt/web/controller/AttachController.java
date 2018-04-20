2448f77e6c1352051b6eb24a738dc0aec668f88f
hpgithubs1 <630519137@qq.com>
2018-04-20 09:16:51 +0800

    修改了Attachcontroller

--- src/main/java/com/gdin/dzzwsyb/swzzbdbxt/web/controller/AttachController.java
+++ src/main/java/com/gdin/dzzwsyb/swzzbdbxt/web/controller/AttachController.java
@@ -1,9 +1,19 @@
 package com.gdin.dzzwsyb.swzzbdbxt.web.controller;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.gdin.dzzwsyb.swzzbdbxt.web.model.Attach;
 
