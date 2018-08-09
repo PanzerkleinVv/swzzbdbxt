7af68143f65a4203c69f2f758c09f9f63d91f44e
hp <hp@hp-PC>
2018-08-09 14:00:54 +0800

    fix bugs

--- src/main/java/com/gdin/dzzwsyb/swzzbdbxt/web/model/MsgQuery.java
+++ src/main/java/com/gdin/dzzwsyb/swzzbdbxt/web/model/MsgQuery.java
@@ -1,8 +1,11 @@
 package com.gdin.dzzwsyb.swzzbdbxt.web.model;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
+
 import com.gdin.dzzwsyb.swzzbdbxt.web.model.MsgExample.Criteria;
 import com.sun.org.apache.xerces.internal.impl.xpath.regex.ParseException;
 
@@ -17,7 +20,7 @@ public class MsgQuery {
 
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date createTimeEnd;
-	
+
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date limitTimeBegin;
 
@@ -33,11 +36,11 @@ public class MsgQuery {
 	private Integer pageNo;
 
 	private Long roleId;
-	
+
 	private Long assistroleId;
 
 	private Long userId;
-	
+
 	private Integer submissionStatus;
 
 	public Date getCreateTimeBegin() {
@@ -103,7 +106,7 @@ public class MsgQuery {
 	public void setRoleId(Long roleId) {
 		this.roleId = roleId;
 	}
-	
+
 	public Long getAssistroleId() {
 		return assistroleId;
 	}
@@ -143,194 +146,82 @@ public class MsgQuery {
 	public void setLimitTimeEnd(Date limitTimeEnd) {
 		this.limitTimeEnd = limitTimeEnd;
 	}
-	
+
 	public void setExample(Criteria criteria) {
 		if (userId != null && userId != 0L) {
 			String condition = " (id in (select msg_id from msg_contractor where user_id=" + userId + ")) ";
 			criteria.addCriterion(condition);
 		}
-		
-		String begin="";
-		String end="";	
-		if(roleId != null) {
-				if(assistroleId != null) { 
-					if(status != null) {	//主办，协办，状态，时间
-						if(limitTimeBegin !=null && limitTimeEnd !=null) { // 检索在2个时间段之间的
-							begin = "'" + MsgQuery.formatDate(limitTimeBegin) +"'" ;
-							end = "'" + MsgQuery.formatDate(limitTimeEnd) +"'" ; 
-							String condition = " (id in (select msg_id from sponsor_co_extend where ( type = 0 and role_id=" + roleId + ")" + " or ( type = 1 and role_id=" + assistroleId + ")" + " and limit_time >=" + begin + " and limit_time <=" +end  + " and status=" + status      + ") ) ";
-							criteria.addCriterion(condition);												
-						}else if (limitTimeBegin !=null) { // 办结期限开始时间
-							begin = "'" + MsgQuery.formatDate(limitTimeBegin) +"'" ;
-							String condition = " (id in (select msg_id from sponsor_co_extend where ( type = 0 and role_id=" + roleId + ")" + " or ( type = 1 and role_id=" + assistroleId + ")" + " and limit_time >=" + begin + " and status=" + status      + ") ) ";
-							criteria.addCriterion(condition);	
-							
-						}else if(limitTimeEnd !=null) { // 办结期限结束时间
-							end = "'" + MsgQuery.formatDate(limitTimeEnd) +"'" ; 
-							String condition = " (id in (select msg_id from sponsor_co_extend where ( type = 0 and role_id=" + roleId + ")" + " or ( type = 1 and role_id=" + assistroleId + ")" + " and limit_time <=" +end  + " and status=" + status      + ") ) ";
-							criteria.addCriterion(condition);	
-						}else if(limitTimeBegin ==null && limitTimeEnd ==null) { // 没有输入办结期限
-							String condition = " (id in (select msg_id from sponsor_co_extend where ( type = 0 and role_id=" + roleId + ")" + " or ( type = 1 and role_id=" + assistroleId + ")" + " and status=" + status      + ") ) ";
-							criteria.addCriterion(condition);	
-						}
-					}else {	//主办，协办，时间
-						if(limitTimeBegin !=null && limitTimeEnd !=null ) { // 检索在2个时间段之间的
-							begin = "'" + MsgQuery.formatDate(limitTimeBegin) +"'" ;
-							end = "'" + MsgQuery.formatDate(limitTimeEnd) +"'" ; 
-							String condition = " (id in (select msg_id from sponsor_co_extend where ( type = 0 and role_id=" + roleId + ")" + " or ( type = 1 and role_id=" + assistroleId + ")" + " and limit_time >=" + begin + " and limit_time <=" +end  + " and status >0 "      + ") ) ";
-							criteria.addCriterion(condition);												
-						}else if(limitTimeBegin !=null) { // 办结期限开始时间
-							begin = "'" + MsgQuery.formatDate(limitTimeBegin) +"'" ;
-							String condition = " (id in (select msg_id from sponsor_co_extend where ( type = 0 and role_id=" + roleId + ")" + " or ( type = 1 and role_id=" + assistroleId + ")" + " and limit_time >=" + begin + " and status >0 "      + ") ) ";
-							criteria.addCriterion(condition);	
-							
-						}else if(limitTimeEnd !=null) { // 办结期限结束时间
-							end = "'" + MsgQuery.formatDate(limitTimeEnd) +"'" ; 
-							String condition = " (id in (select msg_id from sponsor_co_extend where ( type = 0 and role_id=" + roleId + ")" + " or ( type = 1 and role_id=" + assistroleId + ")" + " and limit_time <=" +end  + " and status >0 "      + ") ) ";
-							criteria.addCriterion(condition);	
-						}else if(limitTimeBegin ==null && limitTimeEnd ==null) { // 没有输入办结期限
-							String condition = " (id in (select msg_id from sponsor_co_extend where ( type = 0 and role_id=" + roleId + ")" + " or ( type = 1 and role_id=" + assistroleId + ")" + " and status >0 "      + ") ) ";
-							criteria.addCriterion(condition);	
-						}
-					}
-				}
+
+		if (roleId != null || assistroleId != null || status != null || limitTimeBegin != null || limitTimeEnd != null) {
+			String condition1 = " (id in (select msg_id from msg_sponsor where $$$1)) ";
+			String condition2 =  " (id in (select msg_id from `msg_co-sponsor` where $$$2)) ";
+			List<String> subConditions1 = new ArrayList<String>();
+			List<String> subConditions2 = new ArrayList<String>();
+			if (roleId != null) {
+				subConditions1.add(" role_id = " + roleId + " ");
 			}
-		
-		
-		// 主办，状态，时间  --扩展
-		if(roleId != null) { 
-			if(status != null) { //主办，状态，时间 
-				if(limitTimeBegin !=null && limitTimeEnd !=null && assistroleId == null) { // 检索在2个时间段之间的
-					begin = "'" + MsgQuery.formatDate(limitTimeBegin) +"'" ;
-					end = "'" + MsgQuery.formatDate(limitTimeEnd) +"'" ; 
-					String condition = " (id in (select msg_id from sponsor_co_extend where ( type = 0 and role_id=" + roleId + ")"  + " and limit_time >=" + begin + " and limit_time <=" +end  + " and status=" + status      + ") ) ";
-					criteria.addCriterion(condition);												
-				}else if(limitTimeBegin !=null && assistroleId == null) { // 办结期限开始时间
-					begin = "'" + MsgQuery.formatDate(limitTimeBegin) +"'" ;
-					String condition = " (id in (select msg_id from sponsor_co_extend where ( type = 0 and role_id=" + roleId + ")"  + " and limit_time <=" + begin + " and status=" + status      + ") ) ";
-					criteria.addCriterion(condition);	
-					
-				}else if(limitTimeEnd !=null && assistroleId == null) { // 办结期限结束时间
-					end = "'" + MsgQuery.formatDate(limitTimeEnd) +"'" ; 
-					String condition = " (id in (select msg_id from sponsor_co_extend where ( type = 0 and role_id=" + roleId + ")" + " and limit_time <=" +end  + " and status=" + status      + ") ) ";
-					criteria.addCriterion(condition);	
-				}else if(limitTimeBegin ==null && limitTimeEnd ==null && assistroleId == null) { // 没有输入办结期限
-					String condition = " (id in (select msg_id from sponsor_co_extend where ( type = 0 and role_id=" + roleId + ")"  + " and status=" + status      + ") ) ";
-					criteria.addCriterion(condition);	
-				}
-				
-			}else {  // 主办，时间
-				if(limitTimeBegin !=null && limitTimeEnd !=null && assistroleId == null) { // 检索在2个时间段之间的
-					begin = "'" + MsgQuery.formatDate(limitTimeBegin) +"'" ;
-					end = "'" + MsgQuery.formatDate(limitTimeEnd) +"'" ; 
-					String condition = " (id in (select msg_id from sponsor_co_extend where ( type = 0 and role_id=" + roleId + ")"  + " and limit_time >=" + begin + " and limit_time <=" +end  + " and status > 0"     + ") ) ";
-					criteria.addCriterion(condition);												
-				}else if(limitTimeBegin !=null && assistroleId == null) { // 办结期限开始时间
-					begin = "'" + MsgQuery.formatDate(limitTimeBegin) +"'" ;
-					String condition = " (id in (select msg_id from sponsor_co_extend where ( type = 0 and role_id=" + roleId + ")"  + " and limit_time >=" + begin + " and status > 0"       + ") ) ";
-					criteria.addCriterion(condition);	
-					
-				}else if(limitTimeEnd !=null && assistroleId == null) { // 办结期限结束时间
-					end = "'" + MsgQuery.formatDate(limitTimeEnd) +"'" ; 
-					String condition = " (id in (select msg_id from sponsor_co_extend where ( type = 0 and role_id=" + roleId + ")" + " and limit_time <=" +end  + " and status > 0"       + ") ) ";
-					criteria.addCriterion(condition);	
-				}else if(limitTimeBegin ==null && limitTimeEnd ==null && assistroleId == null) { // 没有输入办结期限
-					String condition = " (id in (select msg_id from sponsor_co_extend where ( type = 0 and role_id=" + roleId + ")"  + " and status > 0"       + ") ) ";
-					criteria.addCriterion(condition);	
-				}
-				
+			if (assistroleId != null) {
+				subConditions2.add(" role_id = " + assistroleId + " ");
 			}
-		
-		}else { // 状态，时间
 			if (status != null) {
-				if(limitTimeBegin !=null && limitTimeEnd !=null && assistroleId == null && roleId == null && status != null) { // 检索在2个时间段之间的
-					begin = "'" + MsgQuery.formatDate(limitTimeBegin) +"'" ;
-					end = "'" + MsgQuery.formatDate(limitTimeEnd) +"'" ; 
-					String condition = " (id in (select msg_id from sponsor_co_extend where"   + " limit_time >=" + begin + " and limit_time <=" +end  + " and status=" + status      + ") ) ";
-					criteria.addCriterion(condition);												
-				}else if(limitTimeBegin !=null && assistroleId == null && roleId == null && status != null) { // 办结期限开始时间
-					begin = "'" + MsgQuery.formatDate(limitTimeBegin) +"'" ;
-					String condition = " (id in (select msg_id from sponsor_co_extend where"   + " limit_time >=" + begin + " and status=" + status      + ") ) ";
-					criteria.addCriterion(condition);	
-					
-				}else if(limitTimeEnd !=null && assistroleId == null && roleId == null && status != null) { // 办结期限结束时间
-					end = "'" + MsgQuery.formatDate(limitTimeEnd) +"'" ; 
-					String condition = " (id in (select msg_id from sponsor_co_extend where"  + " limit_time <=" +end  + " and status=" + status      + ") ) ";
-					criteria.addCriterion(condition);	
-				}else if(limitTimeBegin ==null && limitTimeEnd ==null && assistroleId == null && roleId == null && status != null) { // 没有输入办结期限
-					String condition = " (id in (select msg_id from sponsor_co_extend where"  + " status=" + status      + ") ) ";
-					criteria.addCriterion(condition);	
-				}
-			}else {  // 时间
-				if(limitTimeBegin !=null && limitTimeEnd !=null && assistroleId == null && roleId == null && status == null) { // 检索在2个时间段之间的
-					begin = "'" + MsgQuery.formatDate(limitTimeBegin) +"'" ;
-					end = "'" + MsgQuery.formatDate(limitTimeEnd) +"'" ; 
-					String condition = " (id in (select msg_id from sponsor_co_extend where"   + " limit_time >=" + begin + " and limit_time <=" +end  + " and status > 0"      + ") ) ";
-					criteria.addCriterion(condition);												
-				}else if(limitTimeBegin !=null && assistroleId == null && roleId == null && status == null) { // 办结期限开始时间
-					begin = "'" + MsgQuery.formatDate(limitTimeBegin) +"'" ;
-					String condition = " (id in (select msg_id from sponsor_co_extend where"   + " limit_time >=" + begin + " and status > 0"      + ") ) ";
-					criteria.addCriterion(condition);	
-					
-				}else if(limitTimeEnd !=null && assistroleId == null && roleId == null && status == null) { // 办结期限结束时间
-					end = "'" + MsgQuery.formatDate(limitTimeEnd) +"'" ; 
-					String condition = " (id in (select msg_id from sponsor_co_extend where"  + " limit_time <=" +end  + " and status > 0"     + ") ) ";
-					criteria.addCriterion(condition);	
-				}
-				
+				subConditions1.add(" status = " + status + " ");
+				subConditions2.add(" status = " + status + " ");
 			}
-		
+			if (limitTimeBegin != null) {
+				subConditions1.add(" limit_time >= str_to_date('" + dateToString(limitTimeBegin) + "', '%Y-%m-%d') ");
+				subConditions2.add(" limit_time >= str_to_date('" + dateToString(limitTimeBegin) + "', '%Y-%m-%d') ");
+			}
+			if (!subConditions1.isEmpty()) {
+				String subCondition = subConditions1.get(0);
+				for (int i = 1; i < subConditions1.size(); i++) {
+					subCondition += " and " + subConditions1.get(i);
+				}
+				condition1 = condition1.replace("$$$1", subCondition);
+			}
+			if (!subConditions2.isEmpty()) {
+				String subCondition = subConditions2.get(0);
+				for (int i = 1; i < subConditions2.size(); i++) {
+					subCondition += " and " + subConditions2.get(i);
+				}
+				condition2 = condition2.replace("$$$2", subCondition);
+			}
+			if (condition1.indexOf("$$$1") != -1 && condition2.indexOf("$$$2") == -1) {
+				criteria.addCriterion(condition2);
+			} else if (condition1.indexOf("$$$1") == -1 && condition2.indexOf("$$$2") != -1) {
+				criteria.addCriterion(condition1);
+			} else if (condition1.indexOf("$$$1") == -1 && condition2.indexOf("$$$2") == -1) {
+				criteria.addCriterion(" (" + condition1 + " OR " + condition2 + ") ");
+			} 
 		}
 		
-		// 协办，状态，时间  --扩展
-		if(assistroleId != null) { 
-			if(status != null) { //协办，状态，时间 
-				if(limitTimeBegin !=null && limitTimeEnd !=null && roleId == null) { // 检索在2个时间段之间的
-					begin = "'" + MsgQuery.formatDate(limitTimeBegin) +"'" ;
-					end = "'" + MsgQuery.formatDate(limitTimeEnd) +"'" ; 
-					String condition = " (id in (select msg_id from sponsor_co_extend where ( type = 1 and role_id=" + assistroleId + ")"  + " and limit_time >=" + begin + " and limit_time <=" +end  + " and status=" + status      + ") ) ";
-					criteria.addCriterion(condition);												
-				}else if(limitTimeBegin !=null && roleId == null) { // 办结期限开始时间
-					begin = "'" + MsgQuery.formatDate(limitTimeBegin) +"'" ;
-					String condition = " (id in (select msg_id from sponsor_co_extend where ( type = 1 and role_id=" + assistroleId + ")"  + " and limit_time >=" + begin + " and status=" + status      + ") ) ";
-					criteria.addCriterion(condition);	
-					
-				}else if(limitTimeEnd !=null && roleId == null) { // 办结期限结束时间
-					end = "'" + MsgQuery.formatDate(limitTimeEnd) +"'" ; 
-					String condition = " (id in (select msg_id from sponsor_co_extend where ( type = 1 and role_id=" + assistroleId + ")" + " and limit_time <=" +end  + " and status=" + status      + ") ) ";
-					criteria.addCriterion(condition);	
-				}else if(limitTimeBegin ==null && limitTimeEnd ==null && roleId == null) { // 没有输入办结期限
-					String condition = " (id in (select msg_id from sponsor_co_extend where ( type = 1 and role_id=" + assistroleId + ")"  + " and status=" + status      + ") ) ";
-					criteria.addCriterion(condition);	
-				}
-				
-			}else {  // 协办，时间
-				if(limitTimeBegin !=null && limitTimeEnd !=null && roleId == null ) { // 检索在2个时间段之间的
-					begin = "'" + MsgQuery.formatDate(limitTimeBegin) +"'" ;
-					end = "'" + MsgQuery.formatDate(limitTimeEnd) +"'" ; 
-					String condition = " (id in (select msg_id from sponsor_co_extend where ( type = 1 and role_id=" + assistroleId + ")"  + " and limit_time >=" + begin + " and limit_time <=" +end  + " and status > 0"     + ") ) ";
-					criteria.addCriterion(condition);												
-				}else if(limitTimeBegin !=null && roleId == null) { // 办结期限开始时间
-					begin = "'" + MsgQuery.formatDate(limitTimeBegin) +"'" ;
-					String condition = " (id in (select msg_id from sponsor_co_extend where ( type = 1 and role_id=" + assistroleId + ")"  + " and limit_time >=" + begin + " and status > 0"       + ") ) ";
-					criteria.addCriterion(condition);	
-					
-				}else if(limitTimeEnd !=null && roleId == null) { // 办结期限结束时间
-					end = "'" + MsgQuery.formatDate(limitTimeEnd) +"'" ; 
-					String condition = " (id in (select msg_id from sponsor_co_extend where ( type = 1 and role_id=" + assistroleId + ")" + " and limit_time <=" +end  + " and status > 0"       + ") ) ";
-					criteria.addCriterion(condition);	
-				}
-				else if(limitTimeBegin ==null && limitTimeEnd ==null && roleId == null) { // 没有输入办结期限
-					String condition = " (id in (select msg_id from sponsor_co_extend where ( type = 1 and role_id=" + assistroleId + ")"  + " and status > 0"       + ") ) ";
-					criteria.addCriterion(condition);	
-				}
+		if (assistroleId != null || status != null || limitTimeBegin != null || limitTimeEnd != null) {
+			String condition = " (id in (select msg_id from `msg_co-sponsor` where $$$)) ";
+			List<String> subConditions = new ArrayList<String>();
+			if (assistroleId != null) {
+				subConditions.add(" role_id = " + assistroleId + " ");
 			}
+			if (status != null) {
+				subConditions.add(" status = " + status + " ");
+			}
+			if (limitTimeBegin != null) {
+				subConditions.add(" limit_time >= str_to_date('" + dateToString(limitTimeBegin) + "', '%Y-%m-%d') ");
+			}
+			if (limitTimeEnd != null) {
+				subConditions.add(" limit_time <= str_to_date('" + dateToString(limitTimeEnd) + "', '%Y-%m-%d') ");
+			}
+			String subCondition = subConditions.get(0);
+			for (int i = 1; i < subConditions.size(); i++) {
+				subCondition += " and " + subConditions.get(i);
+			}
+			condition = condition.replace("$$$", subCondition);
+			criteria.addCriterion(condition);
 		}
-		
+
 		if (sequence != null && sequence != 0) {
 			criteria.andSequenceEqualTo(sequence);
 		}
-		
+
 		if (name != null && name.length() > 0) {
 			String[] array = name.split("[\\s]+");
 			String condition = " (name LIKE '%" + array[0] + "%'";
@@ -365,14 +256,17 @@ public class MsgQuery {
 			String condition = "(id in (select msg_id from msg_sponsor where id in (select msg_id from submission where status = 1)) OR id in (select msg_id from `msg_co-sponsor` where id in (select msg_id from submission where status = 1)))";
 			criteria.addCriterion(condition);
 		}
-			
+
 	}
 	
-	
-	public static  String formatDate(Date date)throws ParseException{
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        return sdf.format(date);
-    }
-	
-	
+	private String dateToString(Date date) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		return sdf.format(date);
+	}
+
+	public static String formatDate(Date date) throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		return sdf.format(date);
+	}
+
 }
